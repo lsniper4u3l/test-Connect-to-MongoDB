c4c26c83c1065a8f47c006a5675473ec84606961
Phu <phuaeoof@gmail.com>
2024-11-28 12:23:45 +0700

    first commit

---
+++ locales.js
@@ -0,0 +1,43 @@
+// locales.js
+
+export const translations = {
+    en: {
+        welcome: "👋 Welcome",
+        menu: "🌟 Menu",
+        switchLanguage: "Switch Language",
+
+        error_occurred: "Error Occurred",
+        default_error: "Unable to load data. Please try again later.",
+        try_again: "Try Again",
+
+        loading: "🔄 Loading data... Please wait.",
+        loading_subtext: "The system is preparing your data. Please do not close this page.",
+    
+    },
+    zh: {
+        welcome: "👋 欢迎",
+        menu: "🌟 菜单",
+        switchLanguage: "切换语言",
+
+        error_occurred: "发生错误",
+        default_error: "无法加载数据，请稍后再试。",
+        try_again: "再试一次",
+
+        loading: "🔄 正在加载数据... 请稍等。",
+        loading_subtext: "系统正在为您准备数据，请勿关闭此页面。",
+    
+    },
+    th: {
+        welcome: "👋 ยินดีต้อนรับ",
+        menu: "🌟 เมนู",
+        switchLanguage: "เปลี่ยนภาษา",
+  
+        error_occurred: "เกิดข้อผิดพลาด",
+        default_error: "ไม่สามารถโหลดข้อมูลได้ในขณะนี้ กรุณาลองใหม่อีกครั้ง",
+        try_again: "ลองอีกครั้ง",
+
+        loading: "🔄 กำลังโหลดข้อมูล... โปรดรอสักครู่",
+        loading_subtext: "ระบบกำลังเตรียมข้อมูลให้คุณ โปรดอย่าปิดหน้านี้",
+    
+    },
+};
